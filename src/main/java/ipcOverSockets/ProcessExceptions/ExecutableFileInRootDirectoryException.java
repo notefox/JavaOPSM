197f8d1c1b4a893d4f470270392303dce836a803
notefox <kitsu-geissler@t-online.de>
2020-12-31 14:35:00 +0100

    Testing in everything

---
+++ src/main/java/ipcOverSockets/ProcessExceptions/ExecutableFileInRootDirectoryException.java
@@ -0,0 +1,19 @@
+package ipcOverSockets.ProcessExceptions;
+
+public class ExecutableFileInRootDirectoryException extends Throwable {
+	public ExecutableFileInRootDirectoryException() {
+		super("no unwanted file is supposed to be in the root directory");
+	}
+
+	public ExecutableFileInRootDirectoryException(String message) {
+		super(message);
+	}
+
+	public ExecutableFileInRootDirectoryException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExecutableFileInRootDirectoryException(Throwable cause) {
+		super(cause);
+	}
+}
